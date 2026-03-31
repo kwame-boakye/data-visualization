d3878bf85b3cf4160179c8ce7f9840a1d3c6cdb8
Kelvin Boakye <boakyekelvin306@gmail.com>
2026-03-31 12:33:23 -0500

    cleaned the axis of the random walk

--- rw_visual.py
+++ rw_visual.py
@@ -8,7 +8,14 @@ while True:
 
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
